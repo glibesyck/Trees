125552a6d3a530a03761086f9410bac3cd231d6a
Glib Solodzhuk <glibesyck@MacBook-Pro-Glib.local>
2021-04-29 22:42:53 +0300

    second task

---
+++ Task2/main.py
@@ -0,0 +1,10 @@
+"""
+Main module for demonstrating time difference between finding elements using 
+different approaches.
+CAUTION!!! It may take around hour of your life:))))
+"""
+from linkedbst import LinkedBST
+
+if __name__ == '__main__':
+    tree = LinkedBST()
+    tree.demo_bst('words.txt')
